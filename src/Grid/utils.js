2707e7ff781a702e1a928e21f7edd4258366584b
Richard Hidalgo <Online@Comparas-MacBook-Pro.local>
2017-07-19 18:17:17 -0400

    refactored utils.js: smaller functions

--- src/Grid/utils.js
+++ src/Grid/utils.js
@@ -2,7 +2,28 @@ import PropTypes from 'prop-types';
 import breakpoints from '../breakpoints.json';
 
 const bps = Object.keys(breakpoints);
+
 export const bpPropClass = (bp, prop) => `${prop}On${bp.substr(0, 1).toUpperCase() + bp.substr(1)}`;
+
+const getBooleanClassNames = (props, booleanProps) => booleanProps.reduce(
+  (names, prop) => names.concat(props[prop] ? ` ${prop}` : ''), '');
+
+const getBreakpointClassName = (props, bp) => (props[bp] ? ` ${bp}-${props[bp]}` : '');
+
+const getBreakpointNumericClassName = (props, numericProps, bp) => numericProps.reduce(
+  (names, prop) => {
+    const propValue = props[bpPropClass(bp, prop)];
+    return names.concat(propValue ? ` ${bp}-${prop}-${propValue}` : '');
+  }, '');
+
+const getBreakpointBooleanClassName = (props, booleanProps, bp) => booleanProps.reduce(
+  (names, prop) => names.concat(props[bpPropClass(bp, prop)] ? ` ${bp}-${prop}` : ''), '');
+
+const generateBreakpointClassNames = (props, propNames, bp) => ''.concat(
+  propNames.include ? getBreakpointClassName(props, bp) : '',
+  propNames.numeric ? getBreakpointNumericClassName(props, propNames.numeric, bp) : '',
+  propNames.boolean ? getBreakpointBooleanClassName(props, propNames.boolean, bp) : ''
+);
 
 /**
  * Generate string with classnames from props names and breakpoints
@@ -13,15 +34,27 @@ export const bpPropClass = (bp, prop) => `${prop}On${bp.substr(0, 1).toUpperCase
  * @param {Boolean} hasBreakpoint
  * @returns {String}
  */
-export const generateClassnames = (props, numericProps, booleanProps, hasBreakpoint = false) =>
-  bps.reduce((result, bp) => result.concat(
-    hasBreakpoint && props[bp] ? ` ${bp}-${props[bp]}` : '',
-    numericProps.reduce((names, prop) => {
-      const propValue = props[bpPropClass(bp, prop)];
-      return names.concat(propValue ? ` ${bp}-${prop}-${propValue}` : '');
-    }, ''),
-    booleanProps.reduce((names, prop) => names.concat(props[hasBreakpoint ? bpPropClass(bp, prop) : prop] ? ` ${hasBreakpoint ? `${bp}-` : ''}${prop}` : ''), ''),
-  ), '');
+export const generateClassNames = (props, propNames) => ''.concat(
+  propNames.boolean ? getBooleanClassNames(props, propNames.boolean) : '',
+  propNames.breakpoints ?
+    bps.reduce((className, bp) => className.concat(
+      generateBreakpointClassNames(props, propNames.breakpoints, bp)
+    ), '') : ''
+);
+
+const getBooleanTypes = booleanProps => booleanProps.reduce(
+  (result, prop) => Object.assign(result, { [prop]: PropTypes.bool }), {});
+
+const getBreakpointType = bp => ({ [bp]: PropTypes.number });
+
+const getBreakpointGenericType = (props, bp, type) => props.reduce(
+  (result, prop) => Object.assign(result, { [bpPropClass(bp, prop)]: type }), {});
+
+const generateBreakpointTypes = (propNames, bp) => Object.assign(
+  propNames.include ? getBreakpointType(bp) : {},
+  propNames.numeric ? getBreakpointGenericType(propNames.numeric, bp, PropTypes.number) : {},
+  propNames.boolean ? getBreakpointGenericType(propNames.boolean, bp, PropTypes.bool) : {}
+);
 
 /**
  * Generate object with prop types from props names and breakpoints
@@ -31,13 +64,11 @@ export const generateClassnames = (props, numericProps, booleanProps, hasBreakpo
  * @param {Boolean} hasBreakpoint
  * @returns {Object}
  */
-export const generatePropTypes = (numericProps, booleanProps, hasBreakpoint = false) =>
-  bps.reduce((result, bp) => Object.assign(result, {
-    ...hasBreakpoint && { [bp]: PropTypes.number },
-    ...numericProps.reduce((names, prop) => Object.assign(names, {
-      [bpPropClass(bp, prop)]: PropTypes.number
-    }), {}),
-    ...booleanProps.reduce((names, prop) => Object.assign(names, {
-      [hasBreakpoint ? bpPropClass(bp, prop) : prop]: PropTypes.bool
-    }), {})
-  }), {});
+export const generatePropTypes = (propNames) =>
+  Object.assign(
+    propNames.boolean ? getBooleanTypes(propNames.boolean) : {},
+    bps.reduce((result, bp) => Object.assign(
+      result,
+      generateBreakpointTypes(propNames.breakpoints, bp)
+    ), {})
+  );
