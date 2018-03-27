11fd1a8f3e5ef170c3e9154ce7cb1fdee65da8aa
Esteban Martini <esteban.martini@gmail.com>
2018-03-27 11:02:41 -0300

    add eslint with prettier support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
-  extends: '@comparaonline/eslint-config-react',
-  plugins: [],
+  extends: [
+    '@comparaonline/eslint-config-react',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-confusing-arrow': 0
+  },
   env: {
     browser: true,
     node: true,
     jest: true
-  },
-  rules: {
-    'no-confusing-arrow': 0
   }
 };
