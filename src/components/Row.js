bc287edede2d15f2574e1599199916474ad7d27e
Esteban Martini <esteban.martini@gmail.com>
2018-03-27 10:53:48 -0300

    Add optional vertical gutter (#30)
    * Add addVerticalGutters prop
    
    * Update readme
    
    * Add TS types
    
    * Add prettier file
    
    * 💅 Apply prettier
    
    * Ignore more things
    
    * Add missing default value
    
    * Bump version
    
    * Remove prettier vs eslint rule

--- src/components/Row.js
+++ src/components/Row.js
@@ -12,20 +12,23 @@ export const propNames = {
   boolean: ['collapse', 'expanded']
 };
 
-const Row = (props) => {
+const Row = props => {
   const className = cx(
     `row${generateClassNames(props, propNames)}`,
-    { 'flex-row': props.equalHeightColumns },
+    {
+      'flex-row': props.equalHeightColumns,
+      'v-gutters': props.addVerticalGutters
+    },
     props.className
   );
 
-  return (
-    <div className={className}>
-      {props.children}
-    </div>
-  );
+  return <div className={className}>{props.children}</div>;
 };
 
 Row.propTypes = generatePropTypes(propNames);
+Row.defaultProps = {
+  equalHeightColumns: false,
+  addVerticalGutters: false
+};
 
 export default Row;
---
+++ typings/ui-grid.d.ts
@@ -0,0 +1,61 @@
+import * as React from 'react';
+
+type RowProps = {
+  collapse?: boolean;
+  expanded?: boolean;
+  equalHeightColumns?: boolean;
+  addVerticalGutters?: boolean;
+  collapseOnSmall?: boolean;
+  collapseOnMedium?: boolean;
+  collapseOnLarge?: boolean;
+  collapseOnXlarge?: boolean;
+  collapseOnXxlarge?: boolean;
+  unCollapseOnSmall?: boolean;
+  unCollapseOnMedium?: boolean;
+  unCollapseOnLarge?: boolean;
+  unCollapseOnXlarge?: boolean;
+  unCollapseOnXxlarge?: boolean;
+};
+
+type ColumnsWidth = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+type ColumnProps = {
+  small?: ColumnsWidth;
+  medium?: ColumnsWidth;
+  large?: ColumnsWidth;
+  xlarge?: ColumnsWidth;
+  xxlarge?: ColumnsWidth;
+
+  offsetOnSmall?: boolean;
+  offsetOnMedium?: boolean;
+  offsetOnLarge?: boolean;
+  offsetOnXlarge?: boolean;
+  offsetOnXxlarge?: boolean;
+
+  pushOnSmall?: boolean;
+  pushOnMedium?: boolean;
+  pushOnLarge?: boolean;
+  pushOnXlarge?: boolean;
+  pushOnXxlarge?: boolean;
+
+  pullOnSmall?: boolean;
+  pullOnMedium?: boolean;
+  pullOnLarge?: boolean;
+  pullOnXlarge?: boolean;
+  pullOnXxlarge?: boolean;
+
+  centerOnSmall?: boolean;
+  centerOnMedium?: boolean;
+  centerOnLarge?: boolean;
+  centerOnXlarge?: boolean;
+  centerOnXxlarge?: boolean;
+
+  uncenterOnSmall?: boolean;
+  uncenterOnMedium?: boolean;
+  uncenterOnLarge?: boolean;
+  uncenterOnXlarge?: boolean;
+  uncenterOnXxlarge?: boolean;
+};
+
+export class Row extends React.Component<RowProps> {}
+export class Column extends React.Component<ColumnProps> {}
